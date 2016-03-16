6c774281acda3a472df6d4d06784222c4dc297be
Arjan Frans <arjanfrans.com@gmail.com>
2016-03-16 22:00:02 +0100

    nice

--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import SimpleExample from './components/simple';
-import EventsExample from './components/events';
-import BoundsExample from './components/bounds';
-import VectorLayersExample from './components/vector-layers';
-import OtherLayersExample from './components/other-layers';
-import ZoomControlExample from './components/zoom-control';
-import CustomComponentExample from './components/custom-component';
-import AnimateExample from './components/animate';
+import Board from './components/Board';
 
 const examples = (
   <div>
-    <h1>React-Leaflet examples</h1>
-    <h2>Vector layers</h2>
-    <VectorLayersExample />
+    <h1>Risk on maps</h1>
+    <Board />
   </div>
 );
 
